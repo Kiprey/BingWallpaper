253b62e87ababa3cde818583c68828cd767ef00a
Kiprey <HeyKiprey@outlook.com>
2019-07-17 12:55:32 +0800

    删除不必要提交的文件，并添加异常捕获等功能

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
 
 namespace ChangeWallpapersFromWeb
 {
@@ -18,6 +17,11 @@ namespace ChangeWallpapersFromWeb
             int ScreenHeight = 1080;
             //设置存储位置（绝对路径）
             string DownloadPath = @"E:\BingImg";
+
+            //1920*1080的图片大多数大于200KB，低于200KB的属于API返回错误分辨率的图片。
+            //此API不是Bing官方API，而是第三方API，出错概率大约20%左右
+            //当前分辨率壁纸文件的大致最小大小
+            int ImgSmallestBytes = 200 * 1024;
             
             //初始API --- 随机获取图片
             string RawAPIAddress = "https://bing.ioliu.cn/v1/rand";
@@ -27,13 +31,44 @@ namespace ChangeWallpapersFromWeb
 
             //创建存储图片的文件夹
             System.Console.WriteLine("正在进入壁纸下载目录......");
-            Directory.CreateDirectory(DownloadPath);
+            try
+            {
+                Directory.CreateDirectory(DownloadPath);
+            }
+            catch//这里偷了点懒，直接捕获所有异常
+            {
+                System.Console.WriteLine("无法进入下载目录，可能需要以管理员权限运行");
+                System.Console.WriteLine("\n按任意键退出程序");
+                System.Console.ReadKey();
+                return;
+            }
             //图片路径  图片文件名称实例：2019-07-16 16-31-22-875.jpg
-            string ImgPath = DownloadPath + "//" + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss-fff") + ".jpg";
+            string ImgPath = DownloadPath + "\\" + DateTime.Now.ToString("yyyy-MM-dd HH-mm-ss-fff") + ".jpg";
+
+            DownLoadImg:
 
             //下载图片
             System.Console.WriteLine("正在下载图片......");
-            new WebClient().DownloadFile(ImgAddress, ImgPath);
+            try
+            {
+                new WebClient().DownloadFile(ImgAddress, ImgPath);
+            }
+            catch (WebException)
+            {
+                System.Console.WriteLine("网络连接错误！请在网络良好的环境下重试！");
+                System.Console.WriteLine("\n按任意键退出程序");
+                System.Console.ReadKey();
+                return;
+            }
+            
+
+            if(new FileInfo(ImgPath).Length < ImgSmallestBytes)
+            {
+                System.Console.WriteLine("Error:壁纸WebAPI出错，分发的图片URL不正确！");
+                System.Console.WriteLine("Solution:尝试重新请求并下载......");
+                goto DownLoadImg;
+            }
+            //Main(null);
             
             //return;
             System.Console.WriteLine("正在设置壁纸......");
